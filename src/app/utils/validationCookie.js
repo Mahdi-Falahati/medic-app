8f68b4535e84998fe6319dd70defa6fd31bc7dee
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-11-19 07:02:58 -0800

    add await

--- src/app/utils/validationCookie.js
+++ src/app/utils/validationCookie.js
@@ -1,10 +1,12 @@
 import { VerifyToken } from "./auth";
 
-const validationCookie = (cookieStore) => {
-  const token = cookieStore.get("token");
+const validationCookie = async (cookieStore) => {
+  const cookies = await cookieStore;
+  const token = await cookies.get("token");
   if (!token) {
     return false;
   }
+
   const validation = VerifyToken(token.value, process.env.SECRET_KEY);
 
   if (!validation) {
