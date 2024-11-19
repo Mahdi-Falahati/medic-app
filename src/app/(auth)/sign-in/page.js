9053b8a297aaf0dfa70e5d50de946d98dc51b142
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-11-19 07:10:59 -0800

    check user session for access

--- src/app/(auth)/sign-in/page.js
+++ src/app/(auth)/sign-in/page.js
@@ -1,5 +1,13 @@
 import SignInPage from "@/template/SignInPage";
+import { cookies } from "next/headers";
+import { validationCookie } from "@/utils/validationCookie";
+import { redirect } from "next/navigation";
 
 export default function SignIn() {
+  const cookieStore = cookies();
+  const validation = validationCookie(cookieStore);
+
+  if (validation) redirect("/", "replace");
+
   return <SignInPage />;
 }
--- src/app/(auth)/sign-up/page.js
+++ src/app/(auth)/sign-up/page.js
@@ -1,5 +1,13 @@
 import SignUpPage from "@/template/SignUpPage";
+import { cookies } from "next/headers";
+import { validationCookie } from "@/utils/validationCookie";
+import { redirect } from "next/navigation";
 
 export default function SignUp() {
+  const cookieStore = cookies();
+  const validation = validationCookie(cookieStore);
+
+  if (validation) redirect("/", "replace");
+
   return <SignUpPage />;
 }
