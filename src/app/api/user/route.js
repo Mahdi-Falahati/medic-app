2ac5b25ae70aa794f1b8440bef5f0d3b5b56c33b
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-11-19 07:16:25 -0800

    handle error in api

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -3,18 +3,25 @@ import { validationCookie } from "@/utils/validationCookie";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  const cookieStore = cookies();
-  const validation = validationCookie(cookieStore);
+  try {
+    const cookieStore = cookies();
+    const validation = validationCookie(cookieStore);
 
-  if (!validation) {
+    if (!validation) {
+      return NextResponse.json({
+        error: "شما اجازه دسترسی به این صفحه را ندارید",
+        status: 401,
+      });
+    } else {
+      return NextResponse.json({
+        message: "شما به این صفحه دسترسی دارید",
+        status: 200,
+      });
+    }
+  } catch (error) {
     return NextResponse.json({
-      error: "شما اجازه دسترسی به این صفحه را ندارید",
-      status: 401,
-    });
-  } else {
-    return NextResponse.json({
-      message: "شما به این صفحه دسترسی دارید",
-      status: 200,
+      error: "خطا در بررسی وضعیت کاربر",
+      status: 500,
     });
   }
 }
